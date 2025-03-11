9b6704c37d6821e0e0b4ef979ac991cb6e74c540
HENRY FLORES <floreshenry901@gmail.com>
2025-03-11 15:08:15 -0400

    romanos

--- LABORATORIO-1/hola.cpp
+++ LABORATORIO-1/hola.cpp
@@ -1,1 +1,34 @@
-:void
+#include <iostream>
+#include <string>
+using namespace std;
+
+string solve(int num) {
+    string romano[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    int valores[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    
+    string res = "";
+    
+    for (int i = 0; i < 13; i++) {
+        while (num >= valores[i]) {
+            res += romano[i];
+            num -= valores[i];
+        }
+    }
+    
+    return res;
+}
+
+int main() {
+    int num;
+    
+    cout << "NUMERO POSTV.: ";
+    cin >> num;
+
+    if (num < 1 || num > 3999) {
+        cout << "Por favor, ingresa un número entre 1 y 3999." << endl;
+    } else {
+        cout << " EL NUMERO :  " << num << " EN ROMANO: " << solve(num) << endl;
+    }
+
+    return 0;
+}
